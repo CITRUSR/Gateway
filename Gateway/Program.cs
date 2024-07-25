9375c4fb2a8722c287705242caea842969588c6b
CITRUSR <savelypetrov2007@mail.ru>
2024-07-26 00:08:21 +0300

    Initial commit

---
+++ Gateway/Program.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("Gateway", new OpenApiInfo
+    {
+        Title = "Gateway",
+        Description = "Gateway for dos",
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();

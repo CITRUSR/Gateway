0e0a08ded17035549f0a8956e891cdd5e8a176cd
CITRUSR <savelypetrov2007@mail.ru>
2024-08-18 17:38:24 +0300

    add support enums for swagger as strings

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,7 +1,19 @@
+using System.Text.Json.Serialization;
+using Gateway.Endpoints.UserService.Speciality;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 var app = builder.Build();
 

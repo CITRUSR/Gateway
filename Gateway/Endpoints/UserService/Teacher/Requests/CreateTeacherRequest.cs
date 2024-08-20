64540604a9e7e96cebcd57a0949ac98420a19e98
CITRUSR <savelypetrov2007@mail.ru>
2024-08-20 17:22:45 +0300

    add validation properties for create student endpoint

--- Gateway/Endpoints/UserService/Teacher/Requests/CreateTeacherRequest.cs
+++ Gateway/Endpoints/UserService/Teacher/Requests/CreateTeacherRequest.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gateway.Endpoints.UserService.Teacher.Requests;
 
-public record CreateTeacherRequest(
-    Guid SsoId,
-    string FirstName,
-    string LastName,
-    string? PatronymicName,
-    short RoomId
-);
+public record CreateTeacherRequest
+{
+    Guid SsoId { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string LastName { get; set; }
+
+    [MaxLength(32)]
+    string? PatronymicName { get; set; }
+    short RoomId { get; set; }
+}

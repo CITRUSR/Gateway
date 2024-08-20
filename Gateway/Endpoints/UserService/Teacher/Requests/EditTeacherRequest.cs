e869c9ea6ae1cab80627304e62393ac0682239a5
CITRUSR <savelypetrov2007@mail.ru>
2024-08-20 17:33:08 +0300

    add validation properties for edit teacher endpoint

--- Gateway/Endpoints/UserService/Teacher/Requests/EditTeacherRequest.cs
+++ Gateway/Endpoints/UserService/Teacher/Requests/EditTeacherRequest.cs
@@ -2,12 +2,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Gateway.Endpoints.UserService.Teacher.Requests;
 
-public record EditTeacherRequest(
-    Guid Id,
-    string FirstName,
-    string LastName,
-    string PayronymicName,
-    short RoomId,
-    DateTime? FiredAt,
-    bool IsDeleted
-);
+public record EditTeacherRequest
+{
+    Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string LastName { get; set; }
+
+    [MaxLength(32)]
+    string PayronymicName { get; set; }
+    short RoomId { get; set; }
+    DateTime? FiredAt { get; set; }
+    bool IsDeleted { get; set; }
+}

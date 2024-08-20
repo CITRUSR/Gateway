7b85f5f7fd79bb8981014135d2d6fc4604b4a8fe
CITRUSR <savelypetrov2007@mail.ru>
2024-08-20 17:34:45 +0300

    add validation properties for teacher short info model

--- Gateway/Endpoints/UserService/Teacher/Responses/TeacherShortInfo.cs
+++ Gateway/Endpoints/UserService/Teacher/Responses/TeacherShortInfo.cs
@@ -1,3 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gateway.Endpoints.UserService.Teacher.Responses;
 
-public record TeacherShortInfo(Guid Id, string FirstName, string LastName);
+public record TeacherShortInfo
+{
+    Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string LastName { get; set; }
+}

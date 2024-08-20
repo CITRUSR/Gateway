093ed4424d2aba46911ce547490e155c0e0b3ccc
CITRUSR <savelypetrov2007@mail.ru>
2024-08-20 14:17:17 +0300

    add get students endpoint

--- Gateway/Endpoints/UserService/Student/StudentEndpoints.cs
+++ Gateway/Endpoints/UserService/Student/StudentEndpoints.cs
@@ -1,9 +1,52 @@
+using Gateway.Data.Enums;
+using Gateway.Endpoints.UserService.Student.Enums;
+using Gateway.Endpoints.UserService.Student.Responses;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Gateway.Endpoints.UserService.Student;
 
 public static class StudentEndpoints
 {
     public static IEndpointRouteBuilder Map(IEndpointRouteBuilder builder)
     {
+        string studentTag = "Student";
+
+        builder
+            .MapGet(
+                "api/students",
+                (
+                    [FromQuery] string page,
+                    [FromQuery] string pageSize,
+                    [FromQuery] string? search,
+                    [FromQuery] StudentDroppedOutStatus droppedOutStatus,
+                    [FromQuery] StudentSortState sortState,
+                    [FromQuery] DeletedStatus deletedStatus
+                ) => { }
+            )
+            .Produces<GetStudentsResponse>(StatusCodes.Status200OK)
+            .WithTags(studentTag)
+            .WithOpenApi(operation =>
+            {
+                var droppedOutStatusParameter = operation.Parameters[3];
+                droppedOutStatusParameter.Description =
+                    $"**Default value should be {StudentDroppedOutStatus.OnlyActive}**";
+
+                var sortStateParameter = operation.Parameters[4];
+                sortStateParameter.Description =
+                    $"**Default value should be {StudentSortState.LastNameAsc}**";
+
+                var deletedStatusParameter = operation.Parameters[5];
+                deletedStatusParameter.Description =
+                    $"**Default value should be {DeletedStatus.OnlyActive}**";
+
+                operation.Summary = "Get students";
+                operation.Description =
+                    "Get students with pagination, filtering, sorting, and deleted status."
+                    + "\n\n**Request example:** `/api/groups?page=5&pageSize=10&droppedOutStatus=OnlyActive&sortState=LastNameAsc&deletedStatus=OnlyActive`";
+
+                return operation;
+            });
+
         return builder;
     }
 }

c54e4c423b3b96b35aae22fbc0b11d4c568b6909
CITRUSR <savelypetrov2007@mail.ru>
2024-08-20 17:37:40 +0300

    add validation properties for teacher dto

--- Gateway/Data/Dtos/TeacherDto.cs
+++ Gateway/Data/Dtos/TeacherDto.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gateway.Data.Dtos;
 
-public record TeacherDto(
-    Guid Id,
-    Guid SsoId,
-    string FirstName,
-    string LastName,
-    string? PatronymicName,
-    short RoomId,
-    DateTime? FiredAt,
-    bool IsDeleted
-);
+public record TeacherDto
+{
+    Guid Id { get; set; }
+    Guid SsoId { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    string LastName { get; set; }
+
+    [MaxLength(32)]
+    string? PatronymicName { get; set; }
+    short RoomId { get; set; }
+    DateTime? FiredAt { get; set; }
+    bool IsDeleted { get; set; }
+}

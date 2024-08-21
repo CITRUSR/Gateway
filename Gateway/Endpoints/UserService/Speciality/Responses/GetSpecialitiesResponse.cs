b8fdea5ac1006ae2543fe72874cf359be29eeb3f
CITRUSR <savelypetrov2007@mail.ru>
2024-08-21 17:30:12 +0300

    add validation rules for get specialities response

--- Gateway/Endpoints/UserService/Speciality/Responses/GetSpecialitiesResponse.cs
+++ Gateway/Endpoints/UserService/Speciality/Responses/GetSpecialitiesResponse.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gateway.Endpoints.UserService.Speciality.Responses;
 
-public record GetSpecialitiesResponse(int LastPage, List<SpecialityViewModel> Specialities);
+public record GetSpecialitiesResponse(
+    [property: Required] int LastPage,
+    List<SpecialityViewModel> Specialities
+);

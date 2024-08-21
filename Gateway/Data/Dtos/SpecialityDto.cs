f930945445098258a7a6290f29a7bd83e0f32030
CITRUSR <savelypetrov2007@mail.ru>
2024-08-21 18:52:54 +0300

    add rules for speciality dto

--- Gateway/Data/Dtos/SpecialityDto.cs
+++ Gateway/Data/Dtos/SpecialityDto.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Gateway.Data.Dtos;
 
 public record SpecialityDto(
-    int Id,
-    string Name,
-    string Abbreviation,
-    decimal Cost,
-    byte DurationMonths,
-    bool IsDeleted
+    [property: Required] int Id,
+    [property: Required] [property: MaxLength(256)] string Name,
+    [property: Required] [property: MaxLength(10)] string Abbreviation,
+    [property: Required] [property: DefaultValue(0)] decimal Cost,
+    [property: Required] [property: Range(1, byte.MaxValue)] byte DurationMonths,
+    [property: Required] bool IsDeleted
 );

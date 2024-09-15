9252719d6b5de0abbd47055d71032c0898e891e4
CITRUSR <savelypetrov2007@mail.ru>
2024-09-16 00:12:44 +0300

    fix typo in field in SpecialityViewModel from 'Abbreviations' to 'Abbreviation'

--- Gateway/Endpoints/UserService/Speciality/Responses/SpecialityViewModel.cs
+++ Gateway/Endpoints/UserService/Speciality/Responses/SpecialityViewModel.cs
@@ -5,5 +5,5 @@ namespace Gateway.Endpoints.UserService.Speciality.Responses;
 public record SpecialityViewModel(
     [property: Required] int Id,
     [property: Required] [property: MaxLength(256)] string Name,
-    [property: Required] [property: MaxLength(10)] string Abbreviations
+    [property: Required] [property: MaxLength(10)] string Abbreviation
 );

30be74672a7a75e5889f26cc373acba516ebead9
CITRUSR <savelypetrov2007@mail.ru>
2025-01-09 22:58:36 +0300

    refactoring startup extension

--- Gateway/Extensions/StartupExtensions.cs
+++ Gateway/Extensions/StartupExtensions.cs
@@ -27,34 +27,12 @@ public static class StartupExtensions
         IConfiguration configuration
     )
     {
-        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-
-        services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        });
-        services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
-        {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        });
-
+        ConfigureLogging();
+        ConfigureSwagger(services);
+        ConfigureJsonOptions(services);
         MapsterConfigure();
-
+        RegisterServices(services);
         AddScheduleServiceGrpcClients(services, configuration);
-
-        services.AddSingleton<ISpecialityService, SpecialityService>();
-        services.AddSingleton<ITeacherService, TeacherService>();
-        services.AddSingleton<IGroupService, GroupService>();
-        services.AddSingleton<IStudentService, StudentService>();
-
-        services.AddSingleton<IColorService, ColorService>();
-        services.AddSingleton<IRoomService, RoomService>();
-        services.AddSingleton<ISubjectService, SubjectService>();
-        services.AddSingleton<ICurrentWeekdayService, CurrentWeekdayService>();
-        services.AddSingleton<IClassService, ClassService>();
     }
 
     public static void ConfigureApplication(this WebApplication app)
@@ -102,6 +80,43 @@ public static class StartupExtensions
         ClassConfig.Configure();
     }
 
+    private static void ConfigureSwagger(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    private static void ConfigureJsonOptions(IServiceCollection services)
+    {
+        services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+        services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton<ISpecialityService, SpecialityService>();
+        services.AddSingleton<ITeacherService, TeacherService>();
+        services.AddSingleton<IGroupService, GroupService>();
+        services.AddSingleton<IStudentService, StudentService>();
+
+        services.AddSingleton<IColorService, ColorService>();
+        services.AddSingleton<IRoomService, RoomService>();
+        services.AddSingleton<ISubjectService, SubjectService>();
+        services.AddSingleton<ICurrentWeekdayService, CurrentWeekdayService>();
+        services.AddSingleton<IClassService, ClassService>();
+    }
+
+    private static void ConfigureLogging()
+    {
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+    }
+
     private static void AddScheduleServiceGrpcClients(
         IServiceCollection services,
         IConfiguration configuration
@@ -115,38 +130,54 @@ public static class StartupExtensions
 
         var serviceUrl = configuration.GetValue<string>("ServiceUrls:ScheduleService");
 
-        services
-            .AddGrpcClient<ScheduleServiceClient.ColorService.ColorServiceClient>(options =>
-            {
-                options.Address = new Uri(serviceUrl);
-            })
-            .ConfigurePrimaryHttpMessageHandler(() => handler);
+        if (string.IsNullOrEmpty(serviceUrl))
+        {
+            throw new InvalidOperationException(
+                "Configuration value for 'ServiceUrls:ScheduleService' is missing or empty. "
+                    + "Please ensure the configuration file contains this key."
+            );
+        }
 
-        services
-            .AddGrpcClient<ScheduleServiceClient.RoomService.RoomServiceClient>(options =>
-            {
-                options.Address = new Uri(serviceUrl);
-            })
-            .ConfigurePrimaryHttpMessageHandler(() => handler);
+        AddGrpcClient<ScheduleServiceClient.ColorService.ColorServiceClient>(
+            services,
+            serviceUrl,
+            handler
+        );
 
-        services
-            .AddGrpcClient<ScheduleServiceClient.SubjectService.SubjectServiceClient>(options =>
-            {
-                options.Address = new Uri(serviceUrl);
-            })
-            .ConfigurePrimaryHttpMessageHandler(() => handler);
+        AddGrpcClient<ScheduleServiceClient.RoomService.RoomServiceClient>(
+            services,
+            serviceUrl,
+            handler
+        );
 
-        services
-            .AddGrpcClient<ScheduleServiceClient.CurrentWeekdayService.CurrentWeekdayServiceClient>(
-                options =>
-                {
-                    options.Address = new Uri(serviceUrl);
-                }
-            )
-            .ConfigurePrimaryHttpMessageHandler(() => handler);
+        AddGrpcClient<ScheduleServiceClient.SubjectService.SubjectServiceClient>(
+            services,
+            serviceUrl,
+            handler
+        );
 
+        AddGrpcClient<ScheduleServiceClient.CurrentWeekdayService.CurrentWeekdayServiceClient>(
+            services,
+            serviceUrl,
+            handler
+        );
+
+        AddGrpcClient<ScheduleServiceClient.ClassService.ClassServiceClient>(
+            services,
+            serviceUrl,
+            handler
+        );
+    }
+
+    private static void AddGrpcClient<TClient>(
+        IServiceCollection services,
+        string serviceUrl,
+        HttpClientHandler handler
+    )
+        where TClient : class
+    {
         services
-            .AddGrpcClient<ScheduleServiceClient.ClassService.ClassServiceClient>(options =>
+            .AddGrpcClient<TClient>(options =>
             {
                 options.Address = new Uri(serviceUrl);
             })
